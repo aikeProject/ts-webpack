f7b6f3f4bdb5266fd1f6fe3556997f5a00904523
成雨 <rensailong@zhongan.io>
2019-07-08 21:15:44 +0800

    update

--- public/plugins/ajaxMapping.js
+++
@@ -1,7 +0,0 @@
-var ajaxMapping = function (apiName, model) {
-    console.log('before', apiName, model);
-    if (apiName == 'qqqqqq') {
-    }
-    console.log('after:', apiName, model);
-    return model;
-}
